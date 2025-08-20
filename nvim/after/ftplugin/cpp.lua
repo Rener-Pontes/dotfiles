3ffa5429d4142a87324b0212e00aad21aab78ff2
Rener Pontes <renercpontes@gmail.com>
2025-08-20 10:25:44 -0300

    Setado folding de C++, por algum motivo não funcionou só com o anterior

--- nvim/after/ftplugin/cpp.lua
+++ nvim/after/ftplugin/cpp.lua
@@ -2,3 +2,6 @@ vim.opt_local.tabstop = 4
 vim.opt_local.softtabstop = 0
 vim.opt_local.shiftwidth = 4
 vim.opt_local.expandtab = false
+
+vim.opt_local.foldmethod = 'marker'
+vim.opt_local.foldmarker = '{,}'
